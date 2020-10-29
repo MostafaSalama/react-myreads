90a178c1f3da5c88f090ceea8935e3a8424860ea
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-29 17:11:13 +0200

    create methods for fetching and getting books based on search query

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,18 +1,37 @@
 import React from 'react';
-import {Link} from "react-router-dom";
-function Search() {
-	return (
-		<div className="search-books">
-			<div className="search-books-bar">
-				<Link to={'/'}>
-					<button
-						className="close-search"
-					>
-						Close
-					</button>
-				</Link>
-				<div className="search-books-input-wrapper">
-					{/*
+import { Link } from 'react-router-dom';
+import BookList from "../components/BookList";
+import {search} from "../BooksAPI";
+
+class Search extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { query: '', books: [] };
+	}
+	onChangeHandler = (e)=>{
+		this.setState({query:e.target.value},this.updateBooks) ;
+	}
+	updateBooks(){
+		const {query} = this.state ;
+		search(query).then(res=>{
+			if(res.length){
+				const books = res.filter(res=>res.imageLinks);
+			this.setState({books}) ;
+			}
+		}).catch(err=>{
+			console.log(err) ;
+		})
+	}
+	render() {
+		const {query,books} = this.state ;
+		return (
+			<div className="search-books">
+				<div className="search-books-bar">
+					<Link to={'/'}>
+						<button className="close-search">Close</button>
+					</Link>
+					<div className="search-books-input-wrapper">
+						{/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
@@ -20,13 +39,21 @@ function Search() {
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
-					<input type="text" placeholder="Search by title or author" />
+						<input
+							onChange={this.onChangeHandler}
+							onKeyUp={this.onChangeHandler}
+							value={query}
+							type="text"
+							placeholder="Search by title or author"
+						/>
+					</div>
+				</div>
+				<div className="search-books-results">
+					<BookList shelf={'all'} books={books}/>
 				</div>
 			</div>
-			<div className="search-books-results">
-				<ol className="books-grid"></ol>
-			</div>
-		</div>
-	);
+		);
+	}
 }
+
 export default Search;
