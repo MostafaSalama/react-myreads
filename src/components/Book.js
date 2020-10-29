a35594523c3ac20e53107359194dedf0a3d36c2d
Mostafa Salama <mostafatourad789@gmail.com>
2020-10-29 16:37:19 +0200

    feat: display the book based on props like title and thumbnail

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-function Book() {
+function Book({title,id,shelf,authors,thumbnail}) {
 	return (
 		<div className="book">
 			<div className="book-top">
@@ -9,8 +9,7 @@ function Book() {
 					style={{
 						width: 128,
 						height: 193,
-						backgroundImage:
-							'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")',
+						backgroundImage:`url("${thumbnail}")`,
 					}}
 				/>
 				<div className="book-shelf-changer">
@@ -25,8 +24,10 @@ function Book() {
 					</select>
 				</div>
 			</div>
-			<div className="book-title">To Kill a Mockingbird</div>
-			<div className="book-authors">Harper Lee</div>
+			<div className="book-title">{title}</div>
+			<div className="book-authors">
+                {authors.map(author=><span>{author} <br/></span>)}
+            </div>
 		</div>
 	);
 }
